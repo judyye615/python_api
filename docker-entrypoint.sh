3d8ebe8c9f223d43e67b34d1903b81002b757ef7
zhoudi-ye_agco <zhoudi.ye@agcocorp.com>
2022-07-25 14:15:40 -0500

    mounting edit

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,5 +1,5 @@
 
 echo "Starting api integration test."
 
-pytest -v "/home/docker/code":${{ github.workspace }} -vv ${API_TEST} --html=./reports/report.html
+pytest -v "/home/docker/code":/home/runner/work/python_api/python_api -vv ${API_TEST} --html=./reports/report.html
 
