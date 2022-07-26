bae553380e0f9cbd18c347b343bb59dce48fa43b
zhoudi-ye_agco <zhoudi.ye@agcocorp.com>
2022-07-26 13:20:07 -0500

    edit file

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,3 +1,3 @@
 
 echo "Starting api integration test."
-pytest  -vv ${API_TEST} --html=/home/runner/work/python_api/python_api/reports/report.html
+pytest  -vv ${API_TEST} --html=/home/docker/code/reports/report.html
