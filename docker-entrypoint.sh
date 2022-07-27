09c4f1e652159d25a9e9ae488588f2aedcd3e067
zhoudi-ye_agco <zhoudi.ye@agcocorp.com>
2022-07-27 14:15:48 -0500

    yml changed

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,4 +1,4 @@
 
 echo "Starting api integration test."
 pytest  -vv ${API_TEST} --html=/home/runner/work/python_api/python_api/reports/report.html
-mount /home/docker/code ${{ github.workspace}}
+mount /home/docker/code /home/runner/work/python_api/python_api
